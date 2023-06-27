196f7877da9889eb3dd9c757c7d7befd7132bd22
kill3rstabs <nashitnoorali78@gmail.com>
2023-06-27 12:34:09 +0000

    last commit for hello world page

--- Expressjs/server.js
+++ Expressjs/server.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const app = express()
+const app = express();
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello jello gurld!')
+  res.send('<h1>Hello World!</h1>');
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
